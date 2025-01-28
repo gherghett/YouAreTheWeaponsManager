e3887c8f7cf100bdc6f4a0cbd2845714032607b9
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-28 20:10:35 +0100

    meter for gattling gun

--- Dash/Gattling/gattling_dash.gd
+++ Dash/Gattling/gattling_dash.gd
@@ -3,4 +3,6 @@ extends Node2D
 
 func connect_weapon(weapon):
 	$dash/Button.pressed.connect(weapon.button_pressed)
+	weapon.update_meter.connect($dash/Meter.update_pin)
+	$dash/Meter.max = weapon.max_ammo
 	
---
+++ Dash/Gattling/meter.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var max : int = 100
+var current : int = 0
+@onready var pin_rotation_max = $Pin.rotation_degrees
+#var pin_rotation_current = pin_rotation_zero
+
+func _ready() -> void:
+	update_pin(current)
+
+func update_pin(ammo):
+	current = ammo
+	$Pin.rotation_degrees =  float(current)/float(max) * pin_rotation_max
+	print($Pin.rotation_degrees)
